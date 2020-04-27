f81bd20b26a3cc0a0e29f3755bdf2407377d95ce
Jeonyeeun <wjs4199@naver.com>
2020-04-27 15:08:13 +0900

    create laodfunction

--- manager.c
+++ manager.c
@@ -57,10 +57,24 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+fp=fopen("product.txt","rt");
+if(fp==NULL){
+printf("파일없음\n");
+return 0;
+}
+else {
+while(1){
+fscanf(fp,"%d",&p[count].weight);
+fscanf(fp,"%d",&p[count].price);
+fscanf(fp,"%[^\n]",p[count++].name);
+if(feof(fp)){break;}
+}
+}
 
 
 
 
+fclose(fp);
 	printf("=> 로딩 성공!\n");
-	return count;
+	return count-1;
 }
